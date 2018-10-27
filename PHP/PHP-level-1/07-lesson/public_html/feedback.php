ecb4eee92df0904000a7c6535a26dabbc5d7c1d0
CrashOverride <mrpekapb@gnmail.com>
2018-10-27 18:01:56 +0300

    7 урок

--- PHP/PHP-level-1/07-lesson/public_html/feedback.php
+++ PHP/PHP-level-1/07-lesson/public_html/feedback.php
@@ -10,33 +10,33 @@ require '../engine/core.php';
 
 // вывод шаблона
 echo render('site/feedback', [
-   'feedback' => getItemArray("SELECT * FROM `feedback` ORDER BY id DESC")
+  'feedback' => getItemArray("SELECT * FROM `feedback` ORDER BY id DESC")
 ]);
 
 function sqlInsertFeed()
 {
 
-   $sql = "INSERT INTO `feedback` (`user`, `text`) VALUES ('" . $_POST['name'] . "', '" . $_POST['feed'] . "')";
+  $sql = "INSERT INTO `feedback` (`user`, `text`) VALUES ('" . $_POST['name'] . "', '" . $_POST['feed'] . "')";
 
 
-   if (isset($_POST['name']) || isset($_POST['feed'])) {
+  if (isset($_POST['name']) || isset($_POST['feed'])) {
 
    // Проверяем на ошибки
-      $errors = array();
+    $errors = array();
 
-      if ($_POST['name'] == '') {
-         $errors = 'Введите имя!';
-      }
+    if ($_POST['name'] == '') {
+      $errors = 'Введите имя!';
+    }
 
-      if ($_POST['feed'] == '') {
-         $errors = 'Введите комментарий!';
-      }
+    if ($_POST['feed'] == '') {
+      $errors = 'Введите комментарий!';
+    }
 
-      if (empty($errors)) {
-         execute($sql);
-         header('Location: /feedback.php');
-      } else {
-         echo $errors['0'];
-      }
-   }
+    if (empty($errors)) {
+      execute($sql);
+      header('Location: /feedback.php');
+    } else {
+      echo $errors['0'];
+    }
+  }
 }
--- PHP/PHP-level-1/07-lesson/templates/layouts/layout.php
+++ PHP/PHP-level-1/07-lesson/templates/layouts/layout.php
@@ -11,7 +11,7 @@
    <?php endforeach ?>
 </head>
 <body>
-   
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="/"><?= $config['name'] ?></a>
     <div class="collapse navbar-collapse" id="navbarNav">
@@ -28,7 +28,7 @@
         </ul>
     </div>
 </nav>
- 
+
 <br>
 
 <?= $content ?>
